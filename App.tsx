17bc2fdc6e61fff59e6b266649918e5d9aea9168
Nathanael Jeffrey <naelj15@gmail.com>
2024-07-24 23:10:10 +0700

    added provider to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
-import {Text, View} from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { enableScreens } from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {PaperProvider} from 'react-native-paper';
 import React from 'react';
+import LoginPage from './components/Pages/LoginPage';
+import IncomingJobPage from './components/Pages/IncomingJobPage';
 
+// enableScreens();
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return ( 
-    <View className='h-full w-full bg-black'>
-      <Text className='text-white text-2xl'>asd</Text>
-    </View>
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="login">
+          <Stack.Screen
+            name="login"
+            component={LoginPage}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="incomingJob" component={IncomingJobPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- android/app/src/main/java/com/raksarm/MainActivity.kt
+++ android/app/src/main/java/com/raksarm/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.raksarm
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +12,11 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "raksarm"
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
